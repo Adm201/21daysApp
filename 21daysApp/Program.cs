0fd4c2c9eb8ba81e2969f63d8f3910ea1e365646
AdamW <awieleba3@gmail.com>
2023-01-31 21:35:44 +0100

    Liczy ilość poszczególnych cyfr w podanej liczbie

--- 21daysApp/Program.cs
+++ 21daysApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int[] CharQuantity = new int[10];
+for(int i=0; i < 10; i++)
+{
+    CharQuantity[i] = 0;
+}
+
+string number = Console.ReadLine();
+char[] letter = number.ToArray();
+
+for(int i=0; i<number.Length; i++)
+{
+    for(int j=0; j<10; j++)
+    {
+        string j_string = j.ToString();  char[] x = j_string.ToArray();
+        if (letter[i] == x[0]) { CharQuantity[j]++; j = 10; }
+    }
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Cyfra " + i + " występuje " + CharQuantity[i] + " razy.\n");
+}
