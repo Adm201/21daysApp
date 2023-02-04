47db2bf9fda9331067a68abef02ecca81d419dc1
AdamW <awieleba3@gmail.com>
2023-02-04 23:19:26 +0100

    Naprawa błędu

--- 21daysApp/Program.cs
+++ 21daysApp/Program.cs
@@ -1,24 +1,30 @@
 ﻿
+Console.WriteLine("Podaj liczbe: ");
 
-int[] CharQuantity = new int[10];
-for(int i=0; i < 10; i++)
+
+int[] charQuantity = new int[10];
+
+for(int i = 0; i < 10; i++)
 {
-    CharQuantity[i] = 0;
+    charQuantity[i] = 0;
 }
 
-string number = Console.ReadLine();
-char[] letter = number.ToArray();
+string numberString = Console.ReadLine();
+char[] letter = numberString.ToCharArray();
 
-for(int i=0; i<number.Length; i++)
+for(int i = 0; i < numberString.Length; i++)
 {
-    for(int j=0; j<10; j++)
+    for(int j = 0; j < 10; j++)
     {
-        string j_string = j.ToString();  char[] x = j_string.ToArray();
-        if (letter[i] == x[0]) { CharQuantity[j]++; j = 10; }
+        string jString = j.ToString();
+        if (letter[i] == jString[0]) 
+        { 
+            charQuantity[j]++; 
+        }
     }
 }
 
 for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("Cyfra " + i + " występuje " + CharQuantity[i] + " razy.\n");
+    Console.WriteLine("Cyfra " + i + " występuje " + charQuantity[i] + " razy.\n");
 }
